68114cf58eb8b6de5677bcefa74cbab1886e1900
TylerSilingi <159389095+TylerSilingi@users.noreply.github.com>
2024-03-04 10:40:15 +1300

    Add files via upload
    2770 14 3 tha 14

--- C_04_Dice_simulator.py
+++ C_04_Dice_simulator.py
@@ -1,5 +1,30 @@
 import random
 
+
+# generates an integer between 0 and 6
+# to simulate a roll of a die
+def roll_die():
+    result = random.randint(1, 6)
+    return result
+
+
+# Main routine starts here
+
 for item in range(0, 10):
-    dice_roll = random.randint(1, 3)
-    print(dice_roll, end="\t")
+    user_score = 0
+    double_score = "no"
+
+    # roll two dice
+    roll_1 = roll_die()
+    roll_2 = roll_die()
+
+    # checks if we have a double score opportunity
+    if roll_1 == roll_2:
+        double_score = "yes"
+
+    # find the total points (so far)
+    user_points = roll_1 + roll_2
+
+    # show the user the result
+    print(f"Die 1: {roll_1} \t Die 2: {roll_2} \t Points: {user_points}")
+    print(f"Double score opportunity: {double_score}")
